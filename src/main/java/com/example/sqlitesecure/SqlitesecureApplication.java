c797c8b11ee308f97f2a90afdb7ca7f4851e27c5
wenchenglai <wenchenglai@gmail.com>
2019-04-04 22:53:02 -0400

    Add: README.md file

--- src/main/java/com/example/sqlitesecure/SqlitesecureApplication.java
+++ src/main/java/com/example/sqlitesecure/SqlitesecureApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.sql.*;
+import java.util.function.BiConsumer;
 
 @SpringBootApplication
 public class SqlitesecureApplication implements CommandLineRunner {
@@ -17,36 +18,27 @@ public class SqlitesecureApplication implements CommandLineRunner {
 		Connection connection = null;
 		Statement statement = null;
 
-		try
-		{
+		try {
 			connection = DriverManager.getConnection("jdbc:sqlite:encrypted.db", "", password);
 			statement = connection.createStatement();
 
-			try {
-				statement.execute(String.format("PRAGMA key='%s'", password));
-			} finally {
-				statement.executeUpdate("drop table if exists person");
-				statement.executeUpdate("create table person (id integer, name string)");
-				statement.executeUpdate("insert into person values(1, 'John')");
-				statement.executeUpdate("insert into person values(2, 'Mary')");
-				ResultSet rs = statement.executeQuery("select * from person");
-				while(rs.next())
-				{
-					System.out.println("name = " + rs.getString("name"));
-					System.out.println("id = " + rs.getInt("id"));
-				}
-				statement.close();
-				connection.close();
+			statement.execute(String.format("PRAGMA key='%s'", password));
+			statement.executeUpdate("drop table if exists person");
+			statement.executeUpdate("create table person (id integer, name string)");
+			statement.executeUpdate("insert into person values(1, 'John')");
+			statement.executeUpdate("insert into person values(2, 'Mary')");
+
+			ResultSet rs = statement.executeQuery("select * from person");
+
+			while(rs.next()) {
+				System.out.println("name = " + rs.getString("name"));
+				System.out.println("id = " + rs.getInt("id"));
 			}
-		}
-		catch(SQLException e)
-		{
+
+		} catch(SQLException e) {
 			System.err.println(e.getMessage());
-		}
-		finally
-		{
-			try
-			{
+		} finally {
+			try {
 				if(connection != null) {
 					connection.close();
 				}
@@ -54,9 +46,7 @@ public class SqlitesecureApplication implements CommandLineRunner {
 				if(statement != null) {
 					statement.close();
 				}
-			}
-			catch(SQLException e)
-			{
+			} catch(SQLException e) {
 				System.err.println(e.getMessage());
 			}
 		}
@@ -64,35 +54,25 @@ public class SqlitesecureApplication implements CommandLineRunner {
 
 	private void GetDataFromEncryptedFile(String password) {
 		Connection connection = null;
-		Statement statement = null;
 
-		try
-		{
+		try {
 			connection = DriverManager.getConnection("jdbc:sqlite:encrypted.db", "", password);
-			statement = connection.createStatement();
+			Statement statement = connection.createStatement();
+
 			ResultSet resultSet = statement.executeQuery("SELECT * FROM person");
 			resultSet.next();
 			String name = resultSet.getString("name");
-			System.out.println("Data fetched from SQLite: Result: " + name);
+			System.out.println("Data fetched from encrypted SQLite file: " + name);
 		}
-		catch(SQLException e)
-		{
+		catch (SQLException e) {
 			System.err.println(e.getMessage());
 		}
-		finally
-		{
-			try
-			{
+		finally {
+			try {
 				if(connection != null) {
 					connection.close();
 				}
-
-				if(statement != null) {
-					statement.close();
-				}
-			}
-			catch(SQLException e)
-			{
+			} catch(SQLException e) {
 				System.err.println(e.getMessage());
 			}
 		}
@@ -100,10 +80,12 @@ public class SqlitesecureApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) {
-		CreateEncryptedFile("mypassword");
+		CreateEncryptedFile("good-password");
 
-		GetDataFromEncryptedFile("mypassword");
+		// by providing good password, we could see the data
+		GetDataFromEncryptedFile("good-password");
 
-		GetDataFromEncryptedFile("wrong");
+		// We should fail to get data
+		GetDataFromEncryptedFile("bad-password");
 	}
 }
