90370cd041abec86140c479582196289e914d102
wenchenglai <wenchenglai@gmail.com>
2019-04-06 17:39:03 -0400

    ADD: Performance Tester for encrypted file

--- src/main/java/com/example/sqlitesecure/SqlitesecureApplication.java
+++ src/main/java/com/example/sqlitesecure/SqlitesecureApplication.java
@@ -5,88 +5,92 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.sql.*;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.function.BiConsumer;
 
 @SpringBootApplication
 public class SqlitesecureApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SqlitesecureApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SqlitesecureApplication.class, args);
+    }
 
-	private void CreateEncryptedFile(String password) {
-		Connection connection = null;
-		Statement statement = null;
+    private void CreateEncryptedFile(String password) {
+        Connection connection = null;
+        Statement statement = null;
 
-		try {
-			connection = DriverManager.getConnection("jdbc:sqlite:encrypted.db", "", password);
-			statement = connection.createStatement();
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:encrypted.db", "", password);
+            statement = connection.createStatement();
 
-			statement.execute(String.format("PRAGMA key='%s'", password));
-			statement.executeUpdate("drop table if exists person");
-			statement.executeUpdate("create table person (id integer, name string)");
-			statement.executeUpdate("insert into person values(1, 'John')");
-			statement.executeUpdate("insert into person values(2, 'Mary')");
+            statement.execute(String.format("PRAGMA key='%s'", password));
+            statement.executeUpdate("drop table if exists person");
+            statement.executeUpdate("create table person (id integer, name string)");
+            statement.executeUpdate("insert into person values(1, 'John')");
+            statement.executeUpdate("insert into person values(2, 'Mary')");
 
-			ResultSet rs = statement.executeQuery("select * from person");
+            ResultSet rs = statement.executeQuery("select * from person");
 
-			System.out.println("Created database file successfully, print all the test data.");
-			while(rs.next()) {
-				System.out.println("name = " + rs.getString("name"));
-				System.out.println("id = " + rs.getInt("id"));
-			}
+            System.out.println("Created database file successfully, print all the test data.");
+            while(rs.next()) {
+                System.out.println("name = " + rs.getString("name"));
+                System.out.println("id = " + rs.getInt("id"));
+            }
 
-		} catch(SQLException e) {
-			System.err.println(e.getMessage());
-		} finally {
-			try {
-				if(connection != null) {
-					connection.close();
-				}
+        } catch(SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            try {
+                if(connection != null) {
+                    connection.close();
+                }
 
-				if(statement != null) {
-					statement.close();
-				}
-			} catch(SQLException e) {
-				System.err.println(e.getMessage());
-			}
-		}
-	}
+                if(statement != null) {
+                    statement.close();
+                }
+            } catch(SQLException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
 
-	private void GetDataFromEncryptedFile(String password) {
-		Connection connection = null;
+    private void GetDataFromEncryptedFile(String password) {
+        Connection connection = null;
 
-		try {
-			connection = DriverManager.getConnection("jdbc:sqlite:encrypted.db", "", password);
-			Statement statement = connection.createStatement();
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:encrypted.db", "", password);
+            Statement statement = connection.createStatement();
 
-			ResultSet resultSet = statement.executeQuery("SELECT * FROM person");
-			resultSet.next();
-			String name = resultSet.getString("name");
-			System.out.println("Data fetched from encrypted SQLite file: " + name);
-		}
-		catch (SQLException e) {
-			System.err.println(e.getMessage());
-		}
-		finally {
-			try {
-				if(connection != null) {
-					connection.close();
-				}
-			} catch(SQLException e) {
-				System.err.println(e.getMessage());
-			}
-		}
-	}
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM person");
+            resultSet.next();
+            String name = resultSet.getString("name");
+            System.out.println("Data fetched from encrypted SQLite file: " + name);
+        }
+        catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        finally {
+            try {
+                if(connection != null) {
+                    connection.close();
+                }
+            } catch(SQLException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
 
-	@Override
-	public void run(String... args) {
-		CreateEncryptedFile("good-password");
+    @Override
+    public void run(String... args) {
+        CreateEncryptedFile("good-password");
 
-		// by providing good password, we could see the data
-		GetDataFromEncryptedFile("good-password");
+        // by providing good password, we could see the data
+        GetDataFromEncryptedFile("good-password");
 
-		// We should fail to get data
-		GetDataFromEncryptedFile("bad-password");
-	}
+        // We should fail to get data due to bad password
+        GetDataFromEncryptedFile("bad-password");
+    }
+
+
 }
--- src/test/java/com/example/sqlitesecure/SqlitesecureApplicationTests.java
+++ src/test/java/com/example/sqlitesecure/SqlitesecureApplicationTests.java
@@ -5,6 +5,13 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.Duration;
+import java.time.Instant;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SqlitesecureApplicationTests {
@@ -13,4 +20,62 @@ public class SqlitesecureApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Test
+	public void TestPerformance() {
+		TestCreateNewRegularDb();
+		TestCreateNewEncryptedDb();
+	}
+
+	public static void TestCreateNewEncryptedDb() {
+		GenerateNewDb("encrypted.db", true);
+	}
+
+	public static void TestCreateNewRegularDb() {
+		GenerateNewDb("regular.db", false);
+	}
+
+	private static void GenerateNewDb(String targetDbName, Boolean useEncryption) {
+		Connection connection = null;
+		Instant start = Instant.now();
+
+		try {
+			connection = DriverManager.getConnection("jdbc:sqlite:plaintext.db");
+			Statement statement = connection.createStatement();
+
+
+			String sql = "ATTACH DATABASE '" + targetDbName + "' AS newtarget";
+
+			if (useEncryption) {
+				sql += " KEY 'pass'";
+			}
+
+			statement.execute(sql);
+			statement.execute("create table newtarget.attribTable as SELECT * FROM attribTable;");
+			statement.execute("create table newtarget.blobTable as SELECT * FROM blobTable;");
+			statement.execute("DETACH DATABASE newtarget;");
+			statement.close();
+		}
+		catch(SQLException e)
+		{
+			// if the error message is "out of memory",
+			// it probably means no database file is found
+			System.err.println(e.getMessage());
+		}
+		finally
+		{
+			try
+			{
+				if(connection != null)
+					connection.close();
+			}
+			catch(SQLException e)
+			{
+				// connection close failed.
+				System.err.println(e.getMessage());
+			}
+		}
+		Instant finish = Instant.now();
+		long timeElapsed = Duration.between(start, finish).toMillis();
+		System.out.println("Time elapsed: " + timeElapsed + " ms to generate " + targetDbName);
+	}
 }
